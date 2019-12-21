31d17246d1b56f17705c3d8f4fcfe7532518f5d3
radifanmp <radifanmp@gmail.com>
2019-12-21 15:51:50 +0700

    edit port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express')
 //use express in app variable
 const app = express()
 //define the server port
-const port = 5000
+const port = process.env.PORT || 5000
 //Init Body parser
 const bodyParser = require('body-parser')
 
